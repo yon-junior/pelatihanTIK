fc4e82cabc0ff2edc0117b4d8cfb85d9c944d5c2
yon-junior <lmaopweru@gmail.com>
2025-10-02 12:29:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementById('sapaButton')
+const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function() {
   alert('Halo! Terima kasih sudah berkunjung!');
 });
